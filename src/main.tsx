91bce3061a9fa3cf69ce1e385e38aa063e813ba1
sdileepkumar7462 <sdileepkumar7462@gmail.com>
2026-02-07 22:10:21 +0530

    index changed

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,10 @@
 import { createRoot } from "react-dom/client";
 import { BrowserRouter } from "react-router-dom";
 import App from "./App";
+import "./index.css";
 
 createRoot(document.getElementById("root")!).render(
   <BrowserRouter basename="/tirumalaweddinghalls.github.io">
     <App />
   </BrowserRouter>
 );
-
